19e4fc6f43afc98f2467431318f112ba62cddd0f
djadn0000 <djadn0000@gmail.com>
2022-01-02 11:11:26 -0400

    solucion del proxy borre lo que no servia

--- py/proxyServer.py
+++ py/proxyServer.py
@@ -1,106 +1,152 @@
 import socket
-import sys
-from thread import *
+import threading
+import select
 
 
 
-def main():
-    global listen_port, buffer_size, max_conn
-    try:
-        #preguntar por el puerto donde se esta ejecutando el servidor
-        port = raw_input("Introduzca el puerto de servicio:")
-        listen_port= int(port)
-    except KeyboardInterrupt:
-        sys.exit(0)
-        
-    max_conn = 10
-    buffer_size =8192
-    #
-    try:
-        #inicializando los sockets, resive el request de los clientes y inicia un hilo para devolver el pedido
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind(('', listen_port))
-        s.listen(max_conn)
-        print("[*] Inicializando socket... hecho.")
-        print("[*] Socket atado exitosamente...")
-        print("[*] El servidor inicio exitosamente [{}]".format(listen_port))
-    except Exception as e:
-        print(e)
-        sys.exit(2)
-        
-        
-    while True:
+SOCKS_VERSION = 5
+
+
+class Proxy:
+    def __init__(self):
+        self.username = "username"
+        self.password = "password"
+
+    def handle_client(self, connection):
+        # greeting header
+        # read and unpack 2 bytes from a client
+        version, nmethods = connection.recv(2)
+
+        # get available methods [0, 1, 2]
+        methods = self.get_available_methods(nmethods, connection)
+
+        # accept only USERNAME/PASSWORD auth
+        if 2 not in set(methods):
+            # close connection
+            connection.close()
+            return
+
+        # send welcome message
+        connection.sendall(bytes([SOCKS_VERSION, 2]))
+
+        if not self.verify_credentials(connection):
+            return
+
+        # request (version=5)
+        version, cmd, _, address_type = connection.recv(4)
+
+        if address_type == 1:  # IPv4
+            address = socket.inet_ntoa(connection.recv(4))
+        elif address_type == 3:  # Domain name
+            domain_length = connection.recv(1)[0]
+            address = connection.recv(domain_length)
+            address = socket.gethostbyname(address)
+
+        # convert bytes to unsigned short array
+        port = int.from_bytes(connection.recv(2), 'big', signed=False)
+
         try:
-            conn, addr = s.accept()
-            data = conn.recv(buffer_size)  
-            start_new_thread(conn_string, (conn, data, addr))           
-                
-        except KeyboardInterrupt:
-            s.close()
-            print ("\n[*] Apagando...")
-            sys.exit(1)
-            
-    s.close()
-# esta funcion em devuelve la diereccion del host comunica al buscador  
-def conn_string(conn, data, addr):
-    try:
-        
-        first_line = data.split("\n")[0]
-        url= first_line.split(" ")[1]
-        
-        http_pos = url.find("://")
-        if http_pos == -1:
-            temp = url
-        else:
-            temp = url[(http_pos + 3):]
+            if cmd == 1:  # CONNECT
+                remote = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                remote.connect((address, port))
+                bind_address = remote.getsockname()
+                print("* Connected to {} {}".format(address, port))
+            else:
+                connection.close()
 
-        port_pos = temp.find(":")
-        
-        webserver_pos = temp.find("/")
-        if webserver_pos == -1:
-            webserver_pos =len(temp)
-        webserver = ""
-        port = -1
-        
-        if port_pos == -1 or webserver_pos < port_pos:
-             port = 80
-             webserver= temp[:webserver_pos]
-        else:
-            port= int(temp[(port_pos + 1 ):][:webserver_pos - port_pos -1])
-            webserver = temp[:port_pos]
-        
-        
-        print(webserver)
-        proxy_server(webserver, port, conn, data, addr)
-    except Exception as e:
-        print(e)
+            addr = int.from_bytes(socket.inet_aton(bind_address[0]), 'big', signed=False)
+            port = bind_address[1]
 
- #crea un socket nuevo, se connecta al web server y envia el request al cliente  
-def proxy_server(webserver, port, conn, data, addr):
-    try:
+            reply = b''.join([
+                SOCKS_VERSION.to_bytes(1, 'big'),
+                int(0).to_bytes(1, 'big'),
+                int(0).to_bytes(1, 'big'),
+                int(1).to_bytes(1, 'big'),
+                addr.to_bytes(4, 'big'),
+                port.to_bytes(2, 'big')
+            ])
+        except Exception as e:
+            # return connection refused error
+            reply = self.generate_failed_reply(address_type, 5)
+
+        connection.sendall(reply)
+
+        # establish data exchange
+        if reply[1] == 0 and cmd == 1:
+            self.exchange_loop(connection, remote)
+
+        connection.close()
+
+    
+    def exchange_loop(self, client, remote):
+        while True:
+            # wait until client or remote is available for read
+            r, w, e = select.select([client, remote], [], [])
+
+            if client in r:
+                data = client.recv(4096)
+                if remote.send(data) <= 0:
+                    break
+
+            if remote in r:
+                data = remote.recv(4096)
+                if client.send(data) <= 0:
+                    break
+
+    
+    def generate_failed_reply(self, address_type, error_number):
+        return b''.join([
+            SOCKS_VERSION.to_bytes(1, 'big'),
+            error_number.to_bytes(1, 'big'),
+            int(0).to_bytes(1, 'big'),
+            address_type.to_bytes(1, 'big'),
+            int(0).to_bytes(4, 'big'),
+            int(0).to_bytes(4, 'big')
+        ])
+
+
+    def verify_credentials(self, connection):
+        version = ord(connection.recv(1)) # should be 1
+
+        username_len = ord(connection.recv(1))
+        username = connection.recv(username_len).decode('utf-8')
+
+        password_len = ord(connection.recv(1))
+        password = connection.recv(password_len).decode('utf-8')
+
+        if username == self.username and password == self.password:
+            # success, status = 0
+            response = bytes([version, 0])
+            connection.sendall(response)
+            return True
+
+        # failure, status != 0
+        response = bytes([version, 0xFF])
+        connection.sendall(response)
+        connection.close()
+        return False
+
+
+    def get_available_methods(self, nmethods, connection):
+        methods = []
+        for i in range(nmethods):
+            methods.append(ord(connection.recv(1)))
+        return methods
+
+    def run(self, host, port):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((webserver, port))
-        s.send(data)
+        s.bind((host, port))
+        s.listen()
+
+        print("* Socks5 proxy server is running on {}:{}".format(host, port))
 
         while True:
-            reply = s.recv(buffer_size)
-            
-            if len(reply) > 0:
-                conn.send(reply)
+            conn, addr = s.accept()
+            print("* new connection from {}".format(addr))
+            t = threading.Thread(target=self.handle_client, args=(conn,))
+            t.start()
 
-                dar = float(len(reply))
-                dar = float(dar/1024)
-                dar = "{}.3s".format(dar)
-                print ("[*] El pedido esta hecho: {} => {} <= {}".format(addr[0], dar, webserver))
-            else:
-                break
-            
-        s.close()
-        conn.close()
-    except socket.error (value, msg):
-        s.close()
-        conn.close()
-        sys.exit(1)
-        
+
 if __name__ == "__main__":
-    main()
+    proxy = Proxy()
+    proxy.run("10.0.0.60", 3128)
